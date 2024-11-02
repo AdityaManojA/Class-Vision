afff09751a97471be85e776d2c50ecd75ef84e10
Aditya <adityamanoja@gmail.com>
2024-11-02 12:17:58 +0530

    varsion mismatch imma do this shit again

--- Attendance-System-Face-Recognition-master/recognition/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecognitionConfig(AppConfig):
-    name = 'recognition'
--- Attendance-System-Face-Recognition-master/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Time,Present
-
-# Register your models here.
-admin.site.register(Time)
-admin.site.register(Present)
